a0df9ed88171d3bcb5fd7cd894347f2d1a62140d
Karla Reyes <kmreyes@uc.cl>
2024-09-25 20:01:37 -0300

    entrega taller 6

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,9 +12,20 @@ import { DireccionesModule } from './direcciones/direcciones.module';
 import { CommonMiddleware } from './common/common.middleware';
 import { TiposPersonajesModule } from './tipos-personajes/tipos-personajes.module';
 import { FormulariosModule } from './formularios/formularios.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EquipoModule, UsuariosModule, CervezasModule, CarritoModule, SuscripcionesModule, PedidosModule, PerfilesModule, DireccionesModule, TiposPersonajesModule, FormulariosModule],
+  imports: [ConfigModule.forRoot(
+    {envFilePath: `.env.${process.env.ARCHIVO_ENV}`,
+      load: [() => ({
+      version: require('../package.json').version,
+      name: require('../package.json').name,
+      description: require('../package.json').description,
+      author: require('../package.json').author,
+      license: require('../package.json').license
+      })],
+      isGlobal: true})
+    ,EquipoModule, UsuariosModule, CervezasModule, CarritoModule, SuscripcionesModule, PedidosModule, PerfilesModule, DireccionesModule, TiposPersonajesModule, FormulariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,31 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { CommonInterceptor } from './common/common.interceptor';
 import { CommonFilter } from './common/common.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const configService : ConfigService = app.get(ConfigService);
+  console.log('Port:', configService.get('PORT'));
+  console.log('APP Name:', configService.get('APP_NAME'));
+  console.log('App Versio:', configService.get('APP_VERSION'));
+  console.log('Reg Level:', configService.get('LOG_LEVEL'));
+  console.log('Entorno:', configService.get('AMBIENTE'));
+  const name = configService.get<string>('name');
+  const description = configService.get<string>('description');
+  const version = configService.get<string>('version');
+  const author = configService.get<string>('author');
+  console.log(author);
+  const license = configService.get<string>('licence');
+
+
   const config = new DocumentBuilder()
-    .setTitle('API e-commerce Cervezario Nacional')
-    .setDescription('Esta api describe los endpoints del e-commerce Cervezario Nacional')
-    .setVersion('1.0.0')
+    .setTitle(name + ` - MODULO ${configService.get('AMBIENTE')} `)
+    .setDescription(description)
+    .setVersion(version)
+    .setContact(author,'','')
+    .setLicense(license,'')
     .addTag('Cervezario-Nacional-api')
     .build();
 
@@ -21,6 +39,6 @@ async function bootstrap() {
   app.useGlobalInterceptors(new CommonInterceptor())
   app.useGlobalFilters(new CommonFilter())
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();

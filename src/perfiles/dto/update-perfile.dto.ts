e0335de7f11ae55078ed4503d7f8705b8fef7a85
Andres Fariña <farinavaldivia@gmail.com>
2024-10-24 13:40:38 -0300

    2

--- src/perfiles/dto/update-perfile.dto.ts
+++ src/perfiles/dto/update-perfile.dto.ts
@@ -12,39 +12,37 @@ import { CreateFormularioDto } from 'src/formularios/dto/create-formulario.dto';
 
 export class UpdatePerfileDto extends PartialType(CreatePerfileDto) {
 
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: "El nombre del personaje es requerido para crear un perfil" })
+  @IsString({ message: "el nombre del personaje debe ser un string" })
   @IsEnum(PersonajeCerveza)
   @ApiProperty({ default: 'El Buena Onda (Pale Ale)', description: 'Personaje asociado al perfil basado en las preferencias', enum: PersonajeCerveza })
   public nombre: PersonajeCerveza;
 
-  @IsOptional()
-  @IsArray()
+  @IsOptional({ message: "el histortial es opcional" })
+  @IsArray({ message: "el historial debe ser un array de pedidos" })
   @ValidateNested({ each: true })
-    @Type(() => CreatePedidoDto)
+  @Type(() => CreatePedidoDto)
   @ApiProperty({ default: CreatePedidoDto, description: 'Lista de pedidos asociados al perfil', type: [CreatePedidoDto], })
   public historialPedidos?: Pedido[];
 
-  @IsOptional()
-  @IsString()
-  @ApiProperty({ default: "Plata Premium", description: 'Lista de suscripciones asociadas al perfil', enum: TipoSuscripcion, })
+  @IsOptional({ message: "las suscripciones son opcionales" })
+  @IsString({ message: "las suscripciones deben ser un string" })
+  @ApiProperty({ default: 'Plata Premium', description: 'Lista de suscripciones asociadas al perfil', enum: TipoSuscripcion, })
   public suscripciones?: TipoSuscripcion;
 
-  @IsOptional()
+  @IsOptional({ message: "las recomendaciones son opcionales" })
   @IsEnum(TipoCerveza, { each: true })
-  @ApiProperty({ default: ['Pale Ale'], description: 'Lista de recomendaciones personalizadas para el perfil', enum: TipoCerveza, type: [String], })
+  @ApiProperty({ default: 'Pale Ale', description: 'Lista de recomendaciones personalizadas para el perfil', enum: TipoCerveza, type: [String], })
   public recomendaciones?: TipoCerveza[];
 
-  @IsOptional()
+  @IsOptional({ message: "el formulario de preferencias es opcional" })
   @ValidateNested({ each: true })
-    @Type(() => CreateFormularioDto)
+  @Type(() => CreateFormularioDto)
   @ApiProperty({ default: CreateFormularioDto, description: 'Respuesta del Formulario', type: [CreateFormularioDto] })
-    public formularioPreferencias?: Formulario;
+  public formularioPreferencias?: Formulario;
 
-
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: "el nivel de usuario es requerido" })
+  @IsString({ message: "el nivel de usuario debe ser un string" })
   @ApiProperty({ default: 'Usuario intermedio', description: 'Nivel del usuario (ej: principiante, intermedio, experto)', })
-
   public nivelUsuario: string;
 }

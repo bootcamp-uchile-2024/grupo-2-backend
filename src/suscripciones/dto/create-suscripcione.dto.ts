ea93d7fdbb6df27d104d9e94cc30a0f57f0dbd59
Andres Fariña <farinavaldivia@gmail.com>
2024-10-24 13:42:09 -0300

    2

--- src/suscripciones/dto/create-suscripcione.dto.ts
+++ src/suscripciones/dto/create-suscripcione.dto.ts
@@ -5,21 +5,26 @@ import { TipoEnvio } from "src/enum/tipo-envio";
 import { IsEnum, IsNotEmpty } from "class-validator";
 
 export class CreateSuscripcioneDto {
-    @IsNotEmpty()
+    @IsNotEmpty({message: "el nombre de la suscripcion es requerido"})
     @IsEnum(TipoSuscripcion)
     @ApiProperty({default: "Plata Premium", description:'tipo de suscripcion del usuario', enum:TipoSuscripcion})// === actualizado ===
+    
     public nombre: TipoSuscripcion;
-    @IsNotEmpty()
+    @IsNotEmpty({message: "el precio de la suscripcion es requerido"})
     @ApiProperty({default:12000, description:'precio de la suscripcion'}) // === Actualizado ===
+    
     public precio: number;
-    @IsNotEmpty()
+    @IsNotEmpty({message: "el descuento de la suscripcion es requerido"})
     @ApiProperty({default:"5%", description:'descuento de la suscripcion'})// === Actualizado ===
+    
     public descuento: number;
-    @IsNotEmpty()
+    @IsNotEmpty({message: "el tipo de envio de la suscripcion es requerido"})
     @IsEnum(TipoEnvio)
     @ApiProperty({default: "Express",description:'tipo de envio de la suscripcion', enum: TipoEnvio})// === Actualizado ===
+    
     public tipo_envio: TipoEnvio;// o puede ser enum si definimos los tipos
-    @IsNotEmpty()
+    @IsNotEmpty({message: "el tiempo de envio de la suscripcion es requerido"})
     @ApiProperty({default: "Cerveza1, Cerveza3", description:'lista de cervezas que incluye la suscripcion'})
+    
     public items_promocion: Cerveza[]; //items que contiene la promoción
 }
